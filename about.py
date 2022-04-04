73495cc79058db9acf4f9fe4e25177e375f2c8f9
spsaswat <spsaswat16194@gmail.com>
2022-04-04 19:55:18 +1000

    Updated demo and added about file)

---
+++ about.py
@@ -0,0 +1,4 @@
+# about file
+print("                         PlantDis - A Plant Disease Detector            ")
+print("")
+print("This ML-HUB package detects the disease of the plant by examining a leaf. If image of a leaf is provided then this package will identify what disease it has, if any.")
