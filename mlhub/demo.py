7be6f2bf80e028369f7fc495172eaab562cddd2c
Saswat Panda <45821961+spsaswat@users.noreply.github.com>
2022-04-04 23:33:13 +1000

    if model present already, don't download

--- mlhub/demo.py
+++ mlhub/demo.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-print(tf. __version__)
+print('the tensorflow version is', tf. __version__)
 
 import cv2
 import numpy as np
@@ -12,12 +12,16 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 from tensorflow import keras
+from pathlib import Path
 
-
-url = 'https://drive.google.com/uc?id=1m0bU-NCqzfjO37HFBEm4-2YmTQwQzil6'
-r = requests.get(url, allow_redirects=True)
-output = 'pd_densenet201_6.h5'
-gdown.download(url, output, quiet=False)
+path = Path('pd_densenet201_6.h5')
+if path.is_file():
+    print('Model downloaded already')
+else:
+    url = 'https://drive.google.com/uc?id=1m0bU-NCqzfjO37HFBEm4-2YmTQwQzil6'
+    r = requests.get(url, allow_redirects=True)
+    output = 'pd_densenet201_6.h5'
+    gdown.download(url, output, quiet=False)
 
 diseases=['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','Apple___healthy','Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot','Corn_(maize)___Common_rust','Corn_(maize)___Northern_Leaf_Blight','Corn_(maize)___healthy','Potato___Early_blight',
  'Potato___Late_blight',
@@ -32,7 +36,7 @@ diseases=['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','A
  'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
  'Tomato___Tomato_mosaic_virus',
  'Tomato___healthy']
-print(len(diseases))
+# print(len(diseases))
 
 # Read Images
 img = mpimg.imread('test/CornCommonRust1.JPG')
