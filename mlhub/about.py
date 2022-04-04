1e6dd57cae3dd03af95572364f092dc83bf13d13
Saswat Panda <45821961+spsaswat@users.noreply.github.com>
2022-04-04 22:58:48 +1000

    Delete about.py

--- mlhub/about.py
+++
@@ -1,4 +0,0 @@
-# about file
-print("                         PlantDis - A Plant Disease Detector            ")
-print("")
-print("This ML-HUB package detects the disease of the plant by examining a leaf. If image of a leaf is provided then this package will identify what disease it has, if any.")
