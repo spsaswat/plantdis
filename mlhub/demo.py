c2fe51544f12cf1c969a2be0f340667bd9eb3848
spsaswat <spsaswat16194@gmail.com>
2022-04-27 07:18:39 +1000

    Demo Now performs 3 predictions on randomly chosen images from the test folder

--- mlhub/demo.py
+++ mlhub/demo.py
@@ -12,6 +12,8 @@ import gdown
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+import random
+
 from tensorflow import keras
 from pathlib import Path
 
@@ -22,10 +24,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 warnings.filterwarnings("ignore")
 
 # print('the tensorflow version is', tf. __version__)
-
-
-
 path = Path('pd_densenet201_6.h5')
+
+
 mlcat("PlantDis", """\
     PlantDis is a mlhub package for detecting plant disease from image of a leaf.\n
     This library is based on pretrained-model which itself is based on DenseNet201.\n
@@ -65,31 +66,48 @@ diseases=['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','A
  'Tomato___healthy']
 
 
-# Read Image
-img = mpimg.imread('test/CornCommonRust1.JPG')
-
-# bringing the image to format used for model training 
-img3 = cv2.resize(img,(256,256))
-img4 = np.reshape(img3,[1,256,256,3])
-img4 = img4/255
-
 model = keras.models.load_model('pd_densenet201_6.h5')
-# using the model to predict disease
-disease = np.argmax(model.predict(img4),axis=1)
-# disease is a list and at 0th index is the disease with highest probability 
-# print('The file path is:- test/CornCommonRust1.JPG')
-# print('')
 
-# Splitting the predicted class to plant and disease name.
-plant, dis = diseases[disease[0]].split('___')
-if(dis.lower()=='healthy'):
-  finalAnnot = "Predicted plant is "+plant+" & it is "+dis
-else:
-  finalAnnot = "Predicted plant is "+plant+" & disease is "+dis
-print(finalAnnot)
+# collecting names of images in the test folder
+t_path = Path('test')
+from os import listdir
+from os.path import isfile, join
+onlyfiles = [f for f in listdir(t_path) if isfile(join(t_path, f))]
+from os import walk
+t_img_names = next(walk(t_path), (None, None, []))[2]
 
-# Setting up plt and showing the image used for prediction
-fig = plt.figure("PlantDis Demo")
-plt.title(finalAnnot)
-plt.imshow(img3)
-plt.show()
+# generating 3 unique random numbers
+r_n = random.sample(range(0, 32), 3)
+
+n=3
+for i in range(n):
+  # Read Image
+  img = mpimg.imread('test/'+t_img_names[r_n[i]])
+
+  # bringing the image to format used for model training 
+  img3 = cv2.resize(img,(256,256))
+  img4 = np.reshape(img3,[1,256,256,3])
+  img4 = img4/255
+  # using the model to predict disease
+  disease = np.argmax(model.predict(img4),axis=1)
+  # disease is a list and at 0th index is the disease with highest probability 
+  print('The image Name is',t_img_names[r_n[i]][:-4])
+  # Splitting the predicted class to plant and disease name.
+  plant, dis = diseases[disease[0]].split('___')
+  if(dis.lower()=='healthy'):
+    finalAnnot = "Predicted plant is "+plant+" & it is "+dis
+  else:
+    finalAnnot = "Predicted plant is "+plant+" & disease is "+dis
+  print(finalAnnot)
+  if(i!=(n-1)):
+    print('')
+
+  # Setting up plt and showing the image used for prediction
+  fig = plt.figure("PlantDis Demo")
+  plt.title(finalAnnot)
+  plt.imshow(img3)
+  plt.show()
+
+
+
+
