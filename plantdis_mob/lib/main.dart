70df196dc50c9cebbcf042805ee8ad54cd022261
spsaswat <spsaswat16194@gmail.com>
2022-05-13 07:52:14 +1000

    flutter_mob

---
+++ plantdis_mob/lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('PlantDis'),
+              backgroundColor: Colors.green[600],
+            ),
+            body: Center(child: MyImagePicker())));
+  }
+}
+
+class MyImagePicker extends StatefulWidget {
+  @override
+  MyImagePickerState createState() => MyImagePickerState();
+}
+
+class MyImagePickerState extends State {
+  // File imageURI=File('src');
+  // String result='src';
+  // String path='src';
+  var _image;
+  var path_1;
+  var result;
+
+  Future imageFromCamera() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (image != null) {
+        _image = File(image.path);
+      }
+    });
+  }
+
+  Future imageFromGallery() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (image != null) {
+        _image = File(image.path);
+        path_1 = image.path;
+      }
+    });
+  }
+
+  Future diagnoseLeaf() async {
+    await Tflite.loadModel(
+        model: "assets/pd_tfl_dn_6.tflite", labels: "assets/labels.txt");
+    var output = await Tflite.runModelOnImage(path: path_1);
+
+    setState(() {
+      result = output.toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+        // textStyle: const TextStyle(color: Color(0xff000000)),
+        shadowColor: const Color(0xffF8DC27).withOpacity(0.4),
+        onPrimary: const Color(0xff000000),
+        primary: const Color(0xffF8DC27));
+    return Scaffold(
+        backgroundColor: Colors.lightGreenAccent,
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              _image == null
+                  ? const Text('No image selected.')
+                  : Image.file(_image,
+                      width: 300, height: 200, fit: BoxFit.cover),
+              Container(
+                  margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      IconButton(
+                        icon: const Icon(Icons.camera_alt),
+                        color: const Color(0xffF8DC27),
+                        onPressed: () => imageFromCamera(),
+                      ),
+                      const SizedBox(height: 20),
+                      IconButton(
+                        icon: const Icon(Icons.collections),
+                        color: const Color(0xffF8DC27),
+                        onPressed: () => imageFromGallery(),
+                      ),
+                    ],
+                  )),
+                  Container(
+                      margin: const EdgeInsets.fromLTRB(0, 30, 0, 20),
+                      child: ElevatedButton(
+                        onPressed: () => diagnoseLeaf(),
+                        child: const Text('Diagnose'),
+                        style: style,
+                      )),
+              result == null ? Text('Result') : Text(result)
+            ])));
+  }
+}
