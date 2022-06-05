d3c14427f6c44b78a0f5a7a6a501c6e6d38e287d
spsaswat <spsaswat16194@gmail.com>
2022-06-05 23:04:02 +1000

    Mlhub - Nested Transfer Learning - Oranges

--- mlhub/demo.py
+++ mlhub/demo.py
@@ -23,37 +23,41 @@ from mlhub.pkg import mlask, mlcat
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 warnings.filterwarnings("ignore")
 
-# print('the tensorflow version is', tf. __version__)
-path = Path('pd_densenet201_6.h5')
+model_name = 'pdorft_efficientnetb2_3.h5'
+path = Path(model_name)
 
 
 mlcat("PlantDis", """\
     PlantDis is a mlhub package for detecting plant disease from image of a leaf.\n
-    This library is based on pretrained-model which itself is based on DenseNet201.\n
+    This library is based on pretrained-model which itself is based on EfficientNetB2.\n
     For Details you can visit:-\n
     \nhttps://github.com/spsaswat/plantdis/blob/main/ipynb/\n
     """)
+
+
+# rechecking if the file was successfully downloaded
 if (not path.is_file()):
     mlask(end='\n',
-      prompt="Press Enter to download the model(248MB)")
-    url = 'https://drive.google.com/uc?id=1m0bU-NCqzfjO37HFBEm4-2YmTQwQzil6'
+      prompt="Model Not Found. Press Enter to download the model(130 MB)")
+    url = 'https://drive.google.com/uc?id=1mAxgMNJZ2c_5c16YdAaQWZ5H06BuBAF9'
     r = requests.get(url, allow_redirects=True)
-    output = 'pd_densenet201_6.h5'
+    output = model_name
     gdown.download(url, output, quiet=False)
 # if file size is less than 10MB then it means
 # the file is corrupted, so download again
-elif ((os.path. getsize(path))/1000<10000):
+elif ((os.path.getsize(path) / 1000) < 10000):
   mlask(end='\n',
-    prompt="Press Enter to download the model(248MB)")
-  url = 'https://drive.google.com/uc?id=1m0bU-NCqzfjO37HFBEm4-2YmTQwQzil6'
+    prompt="Model file may be corrupted. Press Enter to download the model(130 MB)")
+  url = 'https://drive.google.com/uc?id=1mAxgMNJZ2c_5c16YdAaQWZ5H06BuBAF9'
   r = requests.get(url, allow_redirects=True)
-  output = 'pd_densenet201_6.h5'
+  output = model_name
   gdown.download(url, output, quiet=False)
 
-
-diseases=['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','Apple___healthy','Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot','Corn_(maize)___Common_rust','Corn_(maize)___Northern_Leaf_Blight','Corn_(maize)___healthy','Potato___Early_blight',
- 'Potato___Late_blight',
- 'Potato___healthy',
+# all the disease classes
+diseases=['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','Apple___healthy',
+ 'Corn___Gray_leaf_spot','Corn___Common_rust','Corn___Northern_Leaf_Blight','Corn___healthy',
+ 'Orange___Citrus_greening',
+ 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
  'Tomato___Bacterial_spot',
  'Tomato___Early_blight',
  'Tomato___Late_blight',
@@ -61,12 +65,12 @@ diseases=['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','A
  'Tomato___Septoria_leaf_spot',
  'Tomato___Spider_mites Two-spotted_spider_mite',
  'Tomato___Target_Spot',
- 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 'Tomato___Yellow_Curl_Virus',
  'Tomato___Tomato_mosaic_virus',
  'Tomato___healthy']
 
 
-model = keras.models.load_model('pd_densenet201_6.h5')
+model = keras.models.load_model(model_name)
 
 # collecting names of images in the test folder
 t_path = Path('test')
@@ -86,19 +90,24 @@ for i in range(n):
   img = mpimg.imread('test/'+t_img_names[r_n[i]])
 
   # bringing the image to format used for model training 
-  img3 = cv2.resize(img,(256,256))
-  img4 = np.reshape(img3,[1,256,256,3])
-  img4 = img4/255
+  # resizing to match input shape of efficientb2
+  img3 = cv2.resize(img,(260,260))
+  # expanding dimension
+  img4 = np.reshape(img3,[1,260,260,3])
+
   # using the model to predict disease
   disease = np.argmax(model.predict(img4),axis=1)
   # disease is a list and at 0th index is the disease with highest probability 
   print('The image Name is',t_img_names[r_n[i]][:-4])
   # Splitting the predicted class to plant and disease name.
   plant, dis = diseases[disease[0]].split('___')
+  actual = 'Actual:- ' + t_img_names[r_n[i]][:-4]
+  # Splitting the predicted class to plant and disease name.
+  plant, dis = diseases[disease[0]].split('___')
   if(dis.lower()=='healthy'):
-    finalAnnot = "Predicted plant is "+plant+" & it is "+dis
+    finalAnnot = actual + '\n'+"Predicted plant is "+plant+" & it is "+dis
   else:
-    finalAnnot = "Predicted plant is "+plant+" & disease is "+dis
+    finalAnnot = actual + '\n'+"Predicted plant is "+plant+" & disease is "+dis
   print(finalAnnot)
   if(i!=(n-1)):
     print('')
--- mlhub/diagnose.py
+++ mlhub/diagnose.py
@@ -17,13 +17,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-v","--view",action="store_true")
 parser.add_argument("file_path")
 args = parser.parse_args()
-# storing the file path in a variable
+
+# regular expression for proper image name with extension
 pattern = "[^\\s]+(.*?)\\.(jpg|jpeg|png|gif)$"
+
+# storing the file path in a variable
 f_path = args.file_path
-# if('/' not in f_path):
+
+# checking if the image has proper extensions
 if(not re.search(pattern,f_path.lower())):
     raise Exception("Please add proper image extension")
-    # f_path = 'test/' + f_path
 
 # checking if the file exists
 assert os.path.exists(f_path), "The file could not be found, "+str(f_path)
@@ -55,27 +58,32 @@ warnings.filterwarnings("ignore")
 
 
 
-path = Path('pd_densenet201_6.h5')
+model_name = 'pdorft_efficientnetb2_3.h5'
+path = Path(model_name)
+
+# rechecking if the file was successfully downloaded
 if (not path.is_file()):
-    mlask(end='\n',
-      prompt="Press Enter to download the model(248MB)")
-    url = 'https://drive.google.com/uc?id=1m0bU-NCqzfjO37HFBEm4-2YmTQwQzil6'
-    r = requests.get(url, allow_redirects=True)
-    output = 'pd_densenet201_6.h5'
-    gdown.download(url, output, quiet=False)
+  print('Model file could not be found, Downloading again..')
+  url = 'https://drive.google.com/uc?id=1mAxgMNJZ2c_5c16YdAaQWZ5H06BuBAF9'
+  r = requests.get(url, allow_redirects=True)
+  output = model_name
+  gdown.download(url, output, quiet=False)
+  print('')
 # if file size is less than 10MB then it means
 # the file is corrupted, so download again
 elif ((os.path. getsize(path))/1000<10000):
-  mlask(end='\n',
-    prompt="Press Enter to download the model(248MB)")
-  url = 'https://drive.google.com/uc?id=1m0bU-NCqzfjO37HFBEm4-2YmTQwQzil6'
+  print('Model file corrupted, Downloading again..')
+  url = 'https://drive.google.com/uc?id=1mAxgMNJZ2c_5c16YdAaQWZ5H06BuBAF9'
   r = requests.get(url, allow_redirects=True)
-  output = 'pd_densenet201_6.h5'
+  output = model_name
   gdown.download(url, output, quiet=False)
+  print('')
 
-diseases=['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','Apple___healthy','Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot','Corn_(maize)___Common_rust','Corn_(maize)___Northern_Leaf_Blight','Corn_(maize)___healthy','Potato___Early_blight',
- 'Potato___Late_blight',
- 'Potato___healthy',
+# all the disease classes
+diseases=['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','Apple___healthy',
+ 'Corn___Gray_leaf_spot','Corn___Common_rust','Corn___Northern_Leaf_Blight','Corn___healthy',
+ 'Orange___Citrus_greening',
+ 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
  'Tomato___Bacterial_spot',
  'Tomato___Early_blight',
  'Tomato___Late_blight',
@@ -83,24 +91,23 @@ diseases=['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','A
  'Tomato___Septoria_leaf_spot',
  'Tomato___Spider_mites Two-spotted_spider_mite',
  'Tomato___Target_Spot',
- 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 'Tomato___Yellow_Curl_Virus',
  'Tomato___Tomato_mosaic_virus',
  'Tomato___healthy']
-# print(len(diseases))
 
 
 
 img = mpimg.imread(str(f_path))
-img3 = cv2.resize(img,(256,256))
-img4 = np.reshape(img3,[1,256,256,3])
-img4 = img4/255
-# plt.imshow(img3)
-model = keras.models.load_model('pd_densenet201_6.h5')
+# bringing the image to format used for model training 
+# resizing to match input shape of efficientb2
+img3 = cv2.resize(img,(260,260))
+# expanding dimension
+img4 = np.reshape(img3,[1,260,260,3])
+model = keras.models.load_model(model_name)
 
 # using the model to predict disease
 disease = np.argmax(model.predict(img4),axis=1)
 # disease is a list and at 0th index is the disease with highest probability 
-# print('')
 
 # Splitting the predicted class to plant and disease name.
 plant, dis = diseases[disease[0]].split('___')
