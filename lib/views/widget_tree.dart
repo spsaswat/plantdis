326c126e1e9357028494dd97fc7addbda16f679c
Srikanth Polisetty <polis.srikanth@gmail.com>
2025-03-26 10:46:42 +1100

    Fix camera on web, add avatar handling, resolve merge conflicts

---
+++ lib/views/widget_tree.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: uri_does_not_exist
+import '../utils/web_utils.dart';
+// ignore: uri_does_not_exist
+import 'pages/take_picture_page.dart';
+// ignore: uri_does_not_exist
+import 'pages/home_page.dart';
+// ignore: uri_does_not_exist
+import 'pages/profile_page.dart';
+
+// ... existing code continues ...
