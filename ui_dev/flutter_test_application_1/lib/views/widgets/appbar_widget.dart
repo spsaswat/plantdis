92984697068f7c55d641c1a4c3eeecd54b92d15d
Jardin <u7837495@anu.edu.au>
2025-04-29 13:23:44 +1000

    Linked appbar_widget IconButton to APPN website

--- ui_dev/flutter_test_application_1/lib/views/widgets/appbar_widget.dart
+++ ui_dev/flutter_test_application_1/lib/views/widgets/appbar_widget.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../data/constants.dart';
 import '../../data/notifiers.dart';
 
 class AppbarWidget extends StatelessWidget implements PreferredSizeWidget {
   const AppbarWidget({super.key});
+
+  Future<void> _launchWebsite() async {
+    final Uri url = Uri.parse('https://www.plantphenomics.org.au/');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(
+        url,
+        mode: LaunchMode.externalApplication,
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +30,7 @@ class AppbarWidget extends StatelessWidget implements PreferredSizeWidget {
         IconButton(
           padding: EdgeInsets.all(5.0),
           icon: Icon(Icons.energy_savings_leaf_sharp),
-          onPressed: () => debugPrint("[Redirect to company page?]"),
+          onPressed: _launchWebsite,
         ),
         ValueListenableBuilder(
           valueListenable: isDarkModeNotifier,
@@ -41,6 +54,7 @@ class AppbarWidget extends StatelessWidget implements PreferredSizeWidget {
       ],
     );
   }
+
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
